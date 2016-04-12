bd3e4d9ae863a03bfb2304c46671aebe0229f18b
kevinthon <kevin.thon@gmail.com>
2016-04-12 15:37:12 +0200

    Mer bugfix

--- R/NorgastFigAndelTid.R
+++ R/NorgastFigAndelTid.R
@@ -118,7 +118,11 @@ NorgastFigAndelTid <- function(RegData=0, valgtVar='RELAPAROTOMY', datoFra='2014
   binomkonf <- function(n, N, konfnivaa=0.95) {
     binkonf <- matrix(nrow=2, ncol = length(n))
     for (i in 1:length(n)) {
-      binkonf[,i] <- binom.test(n[i],N[i], alternative = 'two.sided', conf.level = konfnivaa)$conf.int[1:2]
+      if (N[i]>0) {
+        binkonf[,i] <- binom.test(n[i],N[i], alternative = 'two.sided', conf.level = konfnivaa)$conf.int[1:2]
+      } else {
+        binkonf[,i] <- c(0,0)
+      }
     }
     return(invisible(binkonf))
   }
