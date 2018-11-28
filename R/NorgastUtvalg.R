f27a86f6ec256decdc8a05f2635cac3f07fbe13f
kevinthon <kevin.thon@gmail.com>
2018-11-28 15:27:43 +0100

    Små endringer/bugfix i utvalgstekster, endret defaultverdi på maxPRS

--- R/NorgastUtvalg.R
+++ R/NorgastUtvalg.R
@@ -8,7 +8,7 @@
 #' @export
 
 NorgastUtvalg <- function(RegData, datoFra, datoTil, minald, maxald, erMann, elektiv, BMI,
-                             valgtShus='', tilgang='', minPRS=0, maxPRS=2, ASA='', whoEcog='',
+                             valgtShus='', tilgang='', minPRS=0, maxPRS=2.2, ASA='', whoEcog='',
                              forbehandling='', malign=99, fargepalett='BlaaRapp', op_gruppe='', ncsp='')
 {
   # Definerer intersect-operator
@@ -20,10 +20,10 @@ NorgastUtvalg <- function(RegData, datoFra, datoTil, minald, maxald, erMann, ele
   indDato <- which(RegData$OperasjonsDato >= datoFra & RegData$OperasjonsDato <= datoTil)
   indKj <- if (erMann %in% 0:1) {which(RegData$erMann == erMann)} else {indKj <- 1:Ninn}
   indOp_gr <- if (op_gruppe[1] != ''){which(RegData$Op_gr %in% as.numeric(op_gruppe))} else {indOp_gr <- 1:Ninn}
-  indElekt <- if (elektiv %in% c(0,1)){which(RegData$Hastegrad == elektiv)} else {indElekt <- 1:Ninn}
+  indElekt <- if (elektiv %in% c(0,1)){which(RegData$Hastegrad_tid == elektiv)} else {indElekt <- 1:Ninn}
   indBMI <- if (BMI[1] != '') {which(RegData$BMI_kodet %in% as.numeric(BMI))} else {indBMI <- 1:Ninn}
   indTilgang <- if (tilgang[1] != '') {which(RegData$Tilgang %in% as.numeric(tilgang))} else {indTilgang <- 1:Ninn}
-  indPRS <- if ((minPRS>0) | (maxPRS<2)) {which(RegData$PRSScore >= minPRS & RegData$PRSScore <= maxPRS)} else {indPRS <- 1:Ninn}
+  indPRS <- if ((minPRS>0) | (maxPRS<2.2)) {which(RegData$PRSScore >= minPRS & RegData$PRSScore <= maxPRS)} else {indPRS <- 1:Ninn}
   indASA <- if (ASA[1] != '') {which(RegData$ASA %in% as.numeric(ASA))} else {indASA <- 1:Ninn}
   indWHO <- if (whoEcog[1] != '') {which(RegData$WHOECOG %in% as.numeric(whoEcog))} else {indWHO <- 1:Ninn}
   indNCSP <- if (ncsp[1] != '') {which(substr(RegData$Hovedoperasjon, 1, 5) %in% ncsp)} else {indNCSP <- 1:Ninn}
@@ -46,16 +46,16 @@ NorgastUtvalg <- function(RegData, datoFra, datoTil, minald, maxald, erMann, ele
                  if (length(ncsp) > 9) {paste0('  ', paste(ncsp[which(!is.na(ncsp[10:20]))+9], collapse=', '))},
                  if (length(ncsp) > 20) {paste0('  ', paste(ncsp[which(!is.na(ncsp[21:31]))+20], collapse=', '))},
                  if (length(ncsp) > 31) {paste0('  ', paste(ncsp[which(!is.na(ncsp[32:42]))+31], collapse=', '))},
-                 if (elektiv %in% c(0,1)) {paste0('Hastegrad: ', c('Øyeblikkelig hjelp', 'Elektiv kirurgi')[elektiv+1])},
-                 if (BMI[1] != '') {paste0('BMI-gruppe: ', paste(BMI, collapse=','))},
+                 if (elektiv %in% c(0,1)) {paste0('Operasjonstid: ', c('Utenfor normalarbeidstid', 'Innenfor normalarbeidstid')[elektiv+1])},
+                 if (BMI[1] != '') {paste0('BMI-kategori(er): ', paste(RegData$BMI_kategori[match(as.numeric(BMI), RegData$BMI_kodet)], collapse=', '))},
                  if (length(valgtShus)>1) {paste0('Valgte RESH: ', paste(as.character(valgtShus), collapse=', '))},
-                 if (tilgang %in% c(1,2,3,5)) {paste0('Tilgang: ', c('Åpen', 'Laparoskopisk', 'Konvertert', '','Endoskopisk')[as.numeric(tilgang)])},
-                 if ((minPRS>0) | (maxPRS<2)) {paste0('PRS-score fra ', sprintf('%.2f', min(RegData$PRSScore, na.rm=T)), ' til ',
+                 if (tilgang[1] != '') {paste0('Tilgang: ', paste(c('Åpen', 'Laparoskopisk', 'Konvertert', '','Endoskopisk')[as.numeric(tilgang)], collapse = ', '))},
+                 if ((minPRS>0) | (maxPRS<2.2)) {paste0('PRS-score fra ', sprintf('%.2f', min(RegData$PRSScore, na.rm=T)), ' til ',
                           sprintf('%.2f', max(RegData$PRSScore, na.rm=T)))},
                  if (ASA[1] != '') {paste0('ASA-grad: ', paste(ASA, collapse=','))},
                  if (whoEcog[1] != '') {paste0('WHO ECOG score: ', paste(whoEcog, collapse=','))},
-                 if (forbehandling %in% 1:4) {paste0('Onkologisk forbehandling: ',
-                                                     paste(c('Cytostatika', 'Stråleterapi', 'Komb. kjemo/radioterapi', 'Ingen')[forbehandling], collapse = ', '))},
+                 if (forbehandling[1] != '') {paste0('Onkologisk forbehandling: ',
+                                                     paste(c('Cytostatika', 'Stråleterapi', 'Komb. kjemo/radioterapi', 'Ingen')[as.numeric(forbehandling)], collapse = ', '))},
                  if (malign %in% c(0,1)){paste0('Diagnose: ', c('Benign', 'Malign')[malign+1])}
   )
 
