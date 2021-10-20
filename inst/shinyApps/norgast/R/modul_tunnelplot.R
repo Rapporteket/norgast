dc0ebcede769bd701318c0d23ae4a0de5b6fe5e1
kevinthon <kevin.thon@gmail.com>
2021-10-20 09:18:30 +0200

    debug

--- inst/shinyApps/norgast/R/modul_tunnelplot.R
+++ inst/shinyApps/norgast/R/modul_tunnelplot.R
@@ -161,7 +161,7 @@ tunnelplot <- function(input, output, session, reshID, RegData, hvd_session){
   })
   observeEvent(input$plot_hover2, {
     vals$shus <- data.frame(traktdata()$my_data[order(traktdata()$my_data$andel), ], color="blue")
-    vals$shus$color[round(as.numeric(input$plot_hover2$y))] <- "red"
+    if (length(input$plot_hover2$y) > 0) {vals$shus$color[round(as.numeric(input$plot_hover2$y))] <- "red"}
     vals$klikkinfo <- round(as.numeric(input$plot_hover2$y))
     # vals$shus$color[-round(input$plot_hover2$y)] <- "blue"
   }) #shiny::debounce
@@ -247,6 +247,7 @@ tunnelplot <- function(input, output, session, reshID, RegData, hvd_session){
 
   output$hover_info_verbatim3 <- renderPrint({
     print(vals$klikkinfo)
+    print(length(vals$klikkinfo))
   })
 
   output$hover_info_verbatim <- renderUI({
