7500cbe74f03f66fb54c18de29d01d75a869006c
kevinthon <kevin.thon@gmail.com>
2023-11-06 13:47:10 +0100

    Fjerner visning av bruker i datakvalitetsfane i påvente av at brukernavn rettes opp fra hnikt.

--- R/norgast_hj_funk.R
+++ R/norgast_hj_funk.R
@@ -90,12 +90,14 @@ dobbelreg <- function(RegData, skjemaoversikt, usrRole = 'LU', reshID) {
   flere_sammedato$PasientID <- as.numeric(flere_sammedato$PasientID)
   flere_sammedato$ForlopsID <- as.numeric(flere_sammedato$ForlopsID)
   flere_sammedato$AvdRESH <- as.numeric(flere_sammedato$AvdRESH)
-  flere_sammedato <- flere_sammedato[order(flere_sammedato$OperasjonsDato, flere_sammedato$PasientID), ]
+  flere_sammedato <- flere_sammedato[order(flere_sammedato$OperasjonsDato, flere_sammedato$PasientID, decreasing = T), ]
   if (usrRole != 'SC') {
     flere_sammedato <- flere_sammedato[flere_sammedato$AvdRESH == reshID, ]
-    flere_sammedato <- flere_sammedato %>%
-      dplyr::select(-c(OpprettetAv, SistLagretAv))
-    }
+    # flere_sammedato <- flere_sammedato %>%
+    #   dplyr::select(-c(OpprettetAv, SistLagretAv)) # Kommenteres inn når brukernavn er ordnet
+  }
+  flere_sammedato <- flere_sammedato %>%
+    dplyr::select(-c(OpprettetAv, SistLagretAv)) # Foreløpig variant i påvente av at brukernavn fikses
   return(flere_sammedato)
 }
 
